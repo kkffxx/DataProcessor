5d86d5b431250b4967dfdc12f1e475a19a4f3e58
kfx <kamilfeniks@o2.pl>
2018-12-30 17:56:16 +0000

    added fileprocessor class

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace DataProcessor
+{
+    internal class FileProcessor
+    {
+        public string InputFilePath { get; }
+
+        public FileProcessor(string filePath)
+        {
+            InputFilePath = filePath;
+        }
+
+        public void Process()
+        {
+            System.Console.WriteLine($"Begin process of {InputFilePath}");
+            if (!File.Exists(InputFilePath))
+            {
+                System.Console.WriteLine($"ERROR: file {InputFilePath} does not exist.");
+                return;
+            }
+
+        }
+
+        public void CreateDirectory(string dirName)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(InputFilePath) + "\\" + dirName);
+        }
+    }
+}
--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DataProcessor
 {
@@ -10,6 +11,39 @@ namespace DataProcessor
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Parsing command line options");
+
+            var address = Console.ReadLine();
+
+            if (address.Contains("file"))
+            {
+                Console.WriteLine($"Single file {address} selected");
+                ProcessSingleFile(address);
+            }
+            else if (address == "--dir")
+            {
+                var directoryPath = Path.GetDirectoryName(address);
+                Console.WriteLine($"Directory {directoryPath} selected for {address} files");
+                ProcessDircetory(directoryPath, address);
+            }
+            else
+            {
+                Console.WriteLine("Inavlid option");
+            }
+
+            Console.WriteLine("Press Enter to quit.");
+            Console.ReadLine();
+        }
+
+        private static void ProcessDircetory(string directoryPath, string fileType)
+        {
+            
+        }
+
+        private static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
         }
     }
 }
